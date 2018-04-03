401526187c1533a186383cf68feaad128ed3298c
Vishal Neharkar <37950421+neharkarvishal@users.noreply.github.com>
2018-04-04 00:35:46 +0530

    frstCommit

---
+++ WordEditorAPI/src/org/word/editor/api/WordFilter.java
@@ -0,0 +1,16 @@
+package org.word.editor.api;
+
+/**
+ *
+ * @author Jagermeister
+ */
+public interface WordFilter 
+{
+
+    /**
+     *
+     * @param s
+     * @return
+     */
+    String process(String s);
+}
